72d17157e4a7a399af4405cdca449bed20676af1
xyulusha <nikolay.syatkovskiy@mail.ru>
2021-09-03 08:09:16 +0300

    Адекватный нейминг

--- SnakeGit/Form1.cs
+++ SnakeGit/Form1.cs
@@ -12,88 +12,89 @@ namespace SnakeGit
 {
     public partial class Form1 : Form
     {
-        SolidBrush black_brush;
-        SolidBrush green_brush;
-        SolidBrush white_brush;
-        Pen black_pen;
-        Point[] snake;
-        Point apple;
-        Random r;
-        int len = 1;
-        int width, height;
+        private SolidBrush blackBrush;
+        private SolidBrush greenBrush;
+        private SolidBrush whiteBrush;
+        private Pen blackPen;
+        private Point[] snake;
+        private Point apple;
+        private Random random;
+        private int length = 1;
+        private int width;
+        private int height;
+        private string direction = "up";
         public Form1()
         {
-            r = new Random();
-            snake = new Point[10000];
             InitializeComponent();
+            random = new Random();
+            snake = new Point[10000];
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             width = pictureBox1.Width / 10;
             height = pictureBox1.Height / 10;
             snake[0].X = width / 2;
             snake[0].Y = height / 2;
-            white_brush = new SolidBrush(Color.White);
-            green_brush = new SolidBrush(Color.Green);
-            black_brush = new SolidBrush(Color.Black);
-            black_pen = new Pen(Color.Black, 3);
-            apple.X = r.Next(1, width);
-            apple.Y = r.Next(1, height);
+            whiteBrush = new SolidBrush(Color.White);
+            greenBrush = new SolidBrush(Color.Green);
+            blackBrush = new SolidBrush(Color.Black);
+            blackPen = new Pen(Color.Black, 3);
+            apple.X = random.Next(1, width);
+            apple.Y = random.Next(1, height);
         }
-        string direction = "up";
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Graphics graphics = Graphics.FromImage(pictureBox1.Image);
 
-            g.FillRectangle(white_brush, 0, 0, pictureBox1.Width, pictureBox1.Height);
-            g.DrawRectangle(black_pen, 0, 0, width * 10, height * 10);
-            if (len > 4)
-                for (int i = 1; i < len; i++)
-                    for (int j = i + 1; j < len; j++)
-                    {
-                        if (snake[i].X == snake[j].X && snake[i].Y == snake[j].Y)
+            graphics.FillRectangle(whiteBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            graphics.DrawRectangle(blackPen, 0, 0, width * 10, height * 10);
+            if (length > 4)
+            for (int i = 1; i < length; i++)
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (snake[i].X == snake[j].X && snake[i].Y == snake[j].Y)
                         {
-                            len = 3;
+                        length = 3;
                             WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
 
                             wplayer.URL = "oof.mp3";
                             wplayer.controls.play();
                         }
-                    }
+                }
 
-            for (int i = 0; i < len; i++)
+            for (int i = 0; i < length; i++)
             {
                 if (snake[i].X < 0) snake[i].X += width;
                 if (snake[i].X > width) snake[i].X -= width;
                 if (snake[i].Y < 0) snake[i].Y += height;
                 if (snake[i].Y > height) snake[i].Y -= height;
-                g.FillEllipse(black_brush, snake[i].X * 10, snake[i].Y * 10, 10, 10);
+                graphics.FillEllipse(blackBrush, snake[i].X * 10, snake[i].Y * 10, 10, 10);
                 if (apple.X == snake[i].X && apple.Y == snake[i].Y)
                 {
-                    apple.X = r.Next(1, width - 1);
-                    apple.Y = r.Next(1, height - 1);
-                    len++;
+                    apple.X = random.Next(1, width - 1);
+                    apple.Y = random.Next(1, height - 1);
+                    length++;
                     WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
 
                     wplayer.URL = "eating.mp3";
                     wplayer.controls.play();
                 }
             }
-            g.FillEllipse(green_brush, apple.X * 10, apple.Y * 10, 10, 10);
+            graphics.FillEllipse(greenBrush, apple.X * 10, apple.Y * 10, 10, 10);
             if (direction == "up") snake[0].Y -= 1;
             if (direction == "down") snake[0].Y += 1;
             if (direction == "left") snake[0].X -= 1;
             if (direction == "right") snake[0].X += 1;
 
-            if (len > 10000 - 3)
+            if (length > 10000 - 3)
             {
-                len = 10000 - 3;
+                length = 10000 - 3;
             }
-            for (int i = len; i >= 0; i--)
+            for (int i = length; i >= 0; i--)
             {
                 snake[i + 1].X = snake[i].X;
                 snake[i + 1].Y = snake[i].Y;
             }
-            if (len < 4) len++;
+            if (length < 4) length++;
 
             pictureBox1.Invalidate();
         }
